9c26350a337ee7caac6413ff99932d4975c12741
OleksandrKukotin <oleksandr.kukotin@gmail.com>
2026-04-02 23:23:41 +0300

    Added isLazy field for lazy initialization implementation

--- src/main/java/org/github/oleksandrkukotin/core/SnowflakeDefinition.java
+++ src/main/java/org/github/oleksandrkukotin/core/SnowflakeDefinition.java
@@ -5,21 +5,31 @@ public class SnowflakeDefinition {
     private final Class<?> snowflakeClass;
     private final String snowflakeName;
     private final Scope snowflakeScope;
+    private final boolean isLazy;
 
     public SnowflakeDefinition(Class<?> snowflakeClass, String snowflakeName) {
-        this(snowflakeClass, snowflakeName, Scope.SINGLETON);
+        this(snowflakeClass, snowflakeName, Scope.SINGLETON, false);
     }
 
     public SnowflakeDefinition(Class<?> snowflakeClass, String snowflakeName, Scope snowflakeScope) {
+        this(snowflakeClass, snowflakeName, snowflakeScope,false);
+    }
+
+    public SnowflakeDefinition(Class<?> snowflakeClass, String snowflakeName, Scope snowflakeScope, boolean isLazy) {
+        checkConstructorArguments(snowflakeClass, snowflakeName);
+        this.snowflakeClass = snowflakeClass;
+        this.snowflakeName = snowflakeName;
+        this.snowflakeScope = snowflakeScope;
+        this.isLazy = isLazy;
+    }
+
+    private static void checkConstructorArguments(Class<?> snowflakeClass, String snowflakeName) {
         if (snowflakeClass == null) {
             throw new IllegalArgumentException("Snowflake class cannot be null");
         }
         if (snowflakeName == null || snowflakeName.isBlank()) {
             throw new IllegalArgumentException("Snowflake name cannot be null or empty");
         }
-        this.snowflakeClass = snowflakeClass;
-        this.snowflakeName = snowflakeName;
-        this.snowflakeScope = snowflakeScope;
     }
 
     public Class<?> getSnowflakeClass() {
@@ -32,5 +42,9 @@ public class SnowflakeDefinition {
 
     public Scope getSnowflakeScope() {
         return snowflakeScope;
+    }
+
+    public boolean isLazy() {
+        return isLazy;
     }
 }
